61c47f6dd8d23ceee77c3f1e2860ce3fd7ebd2b6
niko <niconikomori@gmail.com>
2022-01-24 16:06:21 -0800

    Style fix

--- services/events/message_create.js
+++ services/events/message_create.js
@@ -4,7 +4,7 @@ export default (message) => {
   /** Validate that this is a Samobot command. */
   if (message.content.length > 2) {
     if (message.content.slice(0, 2) == 's!') {
-      const parameters = message.content.slice(2).split(' ')
+      const parameters = message.content.slice(2).split(' ');
       const command = parameters.shift();
       parseCommand(command, parameters.join(' '), message);
     }
